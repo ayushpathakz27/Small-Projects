1e8fdc0fc922c2f60d920ede106d9477ab5130a5
ayushpathakz27 <pathakrajayush8@gmail.com>
2023-12-11 02:39:30 +0530

    QR

---
+++ QR code generator/index.js
@@ -0,0 +1,38 @@
+
+// 1. Use the inquirer npm package to get user input.
+// 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+// 3. Create a txt file to save the user input using the native fs node module.
+
+
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer 
+    .prompt([
+    {
+        message: "Enter your URL: ",
+        name: "URL",
+    },
+    ])
+    .then((answers) => {
+        const url = answers.URL;
+        var qr_png = qr.image(url);
+        qr_png.pipe(fs.createWriteStream('QR-image.png'));
+    })
+    .catch((error) => {
+        if(error.TtyError){
+            console.log("Prompt couldn't be rendered in the current window");
+        }
+        else{
+            console.log("Something else went wrong");
+        }
+    });
+
+
+
+
+fs.writeFile("message.txt", "This is a message file for QR-project", (err) => {
+    if(err) throw err;
+    console.log("The file has been saved");
+});
